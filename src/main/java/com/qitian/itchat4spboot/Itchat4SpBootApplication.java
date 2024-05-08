91e75459bfc5c2f57c8dbfb8945c063f89546ccf
qitian2021 <942156930@qq.com>
2024-05-08 09:16:45 +0800

    Your commit message here

---
+++ src/main/java/com/qitian/itchat4spboot/Itchat4SpBootApplication.java
@@ -0,0 +1,13 @@
+package com.qitian.itchat4spboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itchat4SpBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Itchat4SpBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qitian/itchat4spboot/Itchat4SpBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qitian.itchat4spboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itchat4SpBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
